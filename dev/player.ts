b2577a6dd804ba3c5450cc68bbae8bbbc2a2c170
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-22 15:31:15 +0200

    feat: :sparkles: Can move player with arrows and shoot with the spacebar

--- dev/player.ts
+++ dev/player.ts
@@ -1,25 +1,66 @@
+import { Bullet } from "./bullet.js";
+
 export class Player {
 
     private x : number = 0;
     private y : number = 0;
     private yspeed : number = 0;
+    public bullets : Bullet[] = [];
     private div : HTMLElement;
 
     constructor() {
         console.log("Player was created");
+
+        window.addEventListener("keydown", (e:KeyboardEvent) => this.onKeyDown(e));
+        window.addEventListener("keyup", (e:KeyboardEvent) => this.onKeyUp(e));
+
         this.create();
     }
 
-    create() {
+    public getRectangle() {
+        return this.div.getBoundingClientRect();
+    }
 
+    private create() {
         this.div = document.createElement("player");
+        this.div.classList.add("player");
         document.body.appendChild(this.div);
 
         // Generate a random y value within the height of the viewport
         this.x = 100;
         this.y = Math.floor(Math.random() * (window.innerHeight - this.div.clientHeight));
-        
-        // Draw the fish on the right coordinate (x, y)
-        this.div.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    }
+
+    public update() {
+        this.y += this.yspeed
+        // Draw the player on the right coordinate (x, y)
+        this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
+    }
+
+    private onKeyDown(e: KeyboardEvent): void {
+        switch (e.key.toUpperCase()) {
+            case "W":
+            case "ARROWUP" :
+                this.yspeed = -5
+                break
+            case "S":
+            case "ARROWDOWN":
+                this.yspeed = 5
+                break
+            case " ":
+                this.bullets.push(new Bullet());
+                break
+        }
+    }
+
+    private onKeyUp(e: KeyboardEvent): void {
+        switch (e.key.toUpperCase()) {
+            case "W":
+            case "S":
+            case "ARROWUP":
+            case "ARROWDOWN":
+                this.yspeed = 0
+                break
+        }
     }
 }
--- docs/js/player.js
+++ docs/js/player.js
@@ -1,17 +1,53 @@
+import { Bullet } from "./bullet.js";
 export class Player {
     constructor() {
         this.x = 0;
         this.y = 0;
         this.yspeed = 0;
+        this.bullets = [];
         console.log("Player was created");
+        window.addEventListener("keydown", (e) => this.onKeyDown(e));
+        window.addEventListener("keyup", (e) => this.onKeyUp(e));
         this.create();
+    }
+    getRectangle() {
+        return this.div.getBoundingClientRect();
     }
     create() {
         this.div = document.createElement("player");
+        this.div.classList.add("player");
         document.body.appendChild(this.div);
         this.x = 100;
         this.y = Math.floor(Math.random() * (window.innerHeight - this.div.clientHeight));
+    }
+    update() {
+        this.y += this.yspeed;
         this.div.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    }
+    onKeyDown(e) {
+        switch (e.key.toUpperCase()) {
+            case "W":
+            case "ARROWUP":
+                this.yspeed = -5;
+                break;
+            case "S":
+            case "ARROWDOWN":
+                this.yspeed = 5;
+                break;
+            case " ":
+                this.bullets.push(new Bullet());
+                break;
+        }
+    }
+    onKeyUp(e) {
+        switch (e.key.toUpperCase()) {
+            case "W":
+            case "S":
+            case "ARROWUP":
+            case "ARROWDOWN":
+                this.yspeed = 0;
+                break;
+        }
     }
 }
 //# sourceMappingURL=player.js.map
