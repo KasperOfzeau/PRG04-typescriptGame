db0c3abe97ddd7b6f20bd32120980b8eb3d78215
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-27 14:41:55 +0200

    build: :construction: Added enemy file

---
+++ dev/enemy.ts
@@ -0,0 +1,26 @@
+export class Enemy {
+
+    private y : number;
+    private x : number; 
+    private div : HTMLElement;
+
+    constructor() {
+
+    }
+
+    private create() {
+
+    }
+
+    public update() {
+
+    }
+
+    public killEnemy() {
+        
+    }
+
+    public getRectangle() {
+        return this.div.getBoundingClientRect();
+    }
+}
---
+++ docs/js/enemy.js
@@ -0,0 +1,14 @@
+export class Enemy {
+    constructor() {
+    }
+    create() {
+    }
+    update() {
+    }
+    killEnemy() {
+    }
+    getRectangle() {
+        return this.div.getBoundingClientRect();
+    }
+}
+//# sourceMappingURL=enemy.js.map
